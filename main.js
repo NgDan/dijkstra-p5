b02008630788e9970a8fb99d8898f34823846832
NgDan <dan.negrusa@experian.com>
2020-08-29 16:52:22 +0200

    added error messages and highlighting

--- main.js
+++ main.js
@@ -7,6 +7,31 @@ const updateDOM = () => {
   ).length;
 };
 
+const hightLightInstructionMsg = () => {
+  const msgElement = document.querySelector('.instructions-wrapper p');
+  scrollTo(0, 0);
+  msgElement.classList.remove('red-bg', 'white-bg');
+  msgElement.classList.add('red-bg');
+  setTimeout(() => {
+    msgElement.classList.remove('red-bg');
+    msgElement.classList.add('white-bg');
+  }, 200);
+};
+
+const updateInstructions = () => {
+  const msgElement = document.querySelector('.instructions-wrapper p');
+  let message = 'Hover over a cell and press "S" to choose a starting point';
+  if (!!grid.hasStartNode && !grid.hasEndNode) {
+    message =
+      'Select a few nodes on the map by using the left click button, then press "E" to choose an ending point';
+  }
+  if (!!grid.hasStartNode && !!grid.hasEndNode) {
+    message =
+      'Make sure all nodes are connected by dragging the slider or adding more nodes and press "D"';
+  }
+  msgElement.innerText = message;
+};
+
 function setup() {
   // frameRate(1);
   height = 800;
@@ -24,12 +49,9 @@ function setup() {
     let radius = this.value * grid.nodeSize;
     grid.radius = radius;
     grid.newNodes.connectNodesWithinRadius(radius);
-
-    // grid.connectNodesWithinRadius(radius);
-    updateDOM();
-
-    // console.log(grid.newNodes.nodes)
   });
+  updateDOM();
+  updateInstructions();
 }
 
 function snapToGrid(position) {
@@ -48,13 +70,22 @@ function hightlightNode() {
 }
 
 function mouseClicked() {
-  if (mouseX < width && mouseY < width && mouseX > 0 && mouseY > 0) {
+  if (
+    mouseX < width &&
+    mouseY < width &&
+    mouseX > 0 &&
+    mouseY > 0 &&
+    grid.hasStartNode
+  ) {
     grid.newNodes.addNode(snapToGrid(mouseX), snapToGrid(mouseY));
     grid.unvisitedNodes.addNode(snapToGrid(mouseX), snapToGrid(mouseY));
 
     grid.newNodes.connectNodesWithinRadius(grid.radius);
     console.log(grid.newEdges.edges);
     updateDOM();
+    updateInstructions();
+  } else {
+    hightLightInstructionMsg();
   }
 }
 
@@ -62,8 +93,8 @@ function keyPressed() {
   // when you press the S key, add start node
   if (keyCode === 83 && !grid.hasStartNode) {
     grid.hasStartNode = true;
-
     grid.newNodes.addNode(snapToGrid(mouseX), snapToGrid(mouseY), true, false);
+    grid.startNode = grid.newNodes.startNode;
     grid.unvisitedNodes.addNode(
       snapToGrid(mouseX),
       snapToGrid(mouseY),
@@ -72,12 +103,12 @@ function keyPressed() {
     );
     grid.newNodes.connectNodesWithinRadius(grid.radius);
     updateDOM();
+    updateInstructions();
   }
 
   // when you press the E key, add end node
-  if (keyCode === 69 && !grid.hasEndNode) {
+  if (keyCode === 69 && !grid.hasEndNode && grid.hasStartNode) {
     grid.hasEndNode = true;
-
     grid.newNodes.addNode(snapToGrid(mouseX), snapToGrid(mouseY), false, true);
     grid.unvisitedNodes.addNode(
       snapToGrid(mouseX),
@@ -87,10 +118,17 @@ function keyPressed() {
     );
     grid.newNodes.connectNodesWithinRadius(grid.radius);
     updateDOM();
+    updateInstructions();
+  } else {
+    hightLightInstructionMsg();
   }
   if (keyCode === 68) {
     grid.shortestPath.edges = {};
-    grid.dijkstra();
+    if (!!grid.hasStartNode && !!grid.hasEndNode) {
+      grid.dijkstra();
+    } else {
+      hightLightInstructionMsg();
+    }
   }
 }
 
